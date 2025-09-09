0929ed34aa413ca9974bf5bb80d080da76502a39
Francisco Toro <francisco.toro.c@gmail.com>
2025-09-09 17:03:48 -0300

    resized icon

--- whatsapp_float_button/__manifest__.py
+++ whatsapp_float_button/__manifest__.py
@@ -1,18 +1,18 @@
 {
     'name': 'WhatsApp Float Button',
-    'version': '18.0.1',
+    'version': '18.0.1.0.0',
     'summary': 'Botón flotante de WhatsApp en todo el sitio web',
     'description': 'Agrega un botón flotante de WhatsApp en la esquina inferior derecha en todo el sitio web.',
     'category': 'Website',
     'author': 'Francisco Toro',
-    'email': 'ftc.odoo.test@gmail.com',
+    'website': 'https://your-website-or-github-link.com',
     'license': 'LGPL-3',
     'price': 19.0,
     'currency': 'USD',
-    'depends': ['base', 'website'],
-    "data": [
-        "views/res_config_settings_view.xml",
-        "views/whatsapp_template.xml",
+    'depends': ['website'],
+    'data': [
+        'views/res_config_settings_view.xml',
+        'views/whatsapp_template.xml',
     ],
     'assets': {
         'web.assets_frontend': [
@@ -20,7 +20,7 @@
         ],
     },
     'images': [
-    	'static/images/banner.png',
+        'static/description/banner.png',
         'static/description/icon.png',
         'static/description/screenshot.png',
         'static/description/screenshot2.png',
